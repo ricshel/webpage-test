8511b3cdb74807cd29964248427c9b68fdcbcfdf
Richard Shelmerdine <richard@richardshelmerdine.com>
2025-09-21 08:48:20 +0100

    slight updates

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from datetime import datetime
 
 actual = {
-    "top6": ["liverpool", "arsenal", "spurs", "bournemouth", "chelsea", "everton"],
+    "top6": ["liverpool", "spurs", "arsenal", "crystal palace", "bournemouth", "chelsea"],
     "bottom3": ["west ham", "villa", "wolves"],
     "cups": {
         "fa cup":           {"winner": "placeholder", "runner_up": "placeholder"},
